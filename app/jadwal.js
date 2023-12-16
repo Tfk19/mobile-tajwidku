d4f5225ce191ed9a5346060ee1055338a3b7c51e
abadah <dinarabadan03@gmail.com>
2023-12-16 22:00:46 +0700

    integrasi api pada jadwal page

--- app/jadwal.js
+++ app/jadwal.js
@@ -1,55 +1,121 @@
-import { Center, Heading, Box, HStack, ScrollView, Text } from "@gluestack-ui/themed";
-import { Link } from "expo-router";
-import { View, Image, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Center, Heading, Box, ScrollView, Text } from "@gluestack-ui/themed";
+import { Link, Stack } from "expo-router";
+import { View, Image, Pressable, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Header } from "../components";
 
 const Jadwal = () => {
+  const [datas, setDatas] = useState([]);
+
+  const fetchData = () => {
+    fetch("https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/surabaya/2023/12.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setDatas(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const Headers = () => {
     return (
-      <View w="100%">
-        <Link
-          href={{
-            pathname: "/jadwal",
-          }}>
-          <Box rounded="$xl" alignItems="center" w="$50" bg="green" flex={1}>
-            <Box position="relative" alignItems="center">
-              <Image
-                resizeMode="contain"
-                role="img"
-                source={require('../assets/jadwals.png')}
-              />
-              <Heading
-                position="absolute"
-                textAlign="center"
-                color="$white"
-              >
-              </Heading>
+      <Box position="relative">
+        <View w="100%">
+          <Link
+            href={{
+              pathname: "surah",
+            }}
+          >
+            <Box rounded="$xl" alignItems="center" w="$50" bg="">
+              <Box position="relative">
+                <Image
+                  resizeMode="contain"
+                  role="img"
+                  source={require('../assets/jadwals.png')}
+                />
+                <Heading
+                  position="absolute"
+                  top="$100"
+                  left="0"
+                  ml="$4"
+                  mb="$4"
+                  color="$white"
+                >
+                </Heading>
+              </Box>
             </Box>
+          </Link>
+        </View>
+      </Box>
+    );
+  };
+
+  const renderItem = ({ item }) => {
+    const newsItem = {
+      id: item.id,
+    };
+
+    return (
+      <Box p="$2" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+        {/* <Text>{item.tanggal}</Text> */}
+        <Pressable>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`Imsyak: ${item.imsyak ? item.imsyak : "N/A"}`}</Text>
           </Box>
-        </Link>
-      </View>
-    )
-  }
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`Shubuh: ${item.shubuh ? item.shubuh : "N/A"}`}</Text>
+          </Box>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`dhuha: ${item.dhuha ? item.dhuha : "N/A"}`}</Text>
+          </Box>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`dzuhur: ${item.dzuhur ? item.dzuhur : "N/A"}`}</Text>
+          </Box>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`ashr: ${item.ashr ? item.ashr : "N/A"}`}</Text>
+          </Box>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`magrib: ${item.magrib ? item.magrib : "N/A"}`}</Text>
+          </Box>
+          <Box bg="$teal" p="$5" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Text color='white'>{`isya: ${item.isya ? item.isya : "N/A"}`}</Text>
+          </Box>
+        </Pressable>
+        {/* <Box bg="$teal" p="$2" paddingHorizontal={10} w={350} mb="$3" rounded={"$md"}>
+            <Pressable >
+              <Heading fontWeight="bold" color="white">
+                {item.ashr}
+              </Heading>
+              <Text color="white">{item.isya}</Text>
+            </Pressable>
+        </Box> */}
+      </Box>
+    );
+  };
 
   return (
     <ScrollView>
-      <Header withBack={true} top="$0" title={"Home"} />
+      <Header withBack={true} title={"Home"} />
+      
       <Center>
-        <View>
-          <Headers />
-        </View>
+        <Headers />
+        <View style={{ height: 20 }} />
         <Heading color="$green800" py="$4" >
-          خيركم من تعلّم القران و علّمه
+          KOTA SURABAYA
         </Heading>
         <Text color="$green700" bottom="$2" px="$10" textAlign="center" sizes="6xl" >
           “Sebaik-baiknya diantara kamu adalah
           orang yang belajar Al-Qur’an dan
           Mengajarkannya”
         </Text>
-        <View>
-
-        </View>
+        <FlatList
+          data={datas.length > 0 ? [datas[0]] : []}
+          renderItem={renderItem}
+        />
       </Center>
     </ScrollView>
   );
